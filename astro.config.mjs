2cb56e5c739df64ad9c8be80bfbaad6a6aeb0d3e
Hayoun Kim <hayoun.k.1102@gmail.com>
2024-08-26 15:47:45 -0700

    github page setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://hayoun-k.github.io',
+  base: 'landing_page_webcosmos',
+});
